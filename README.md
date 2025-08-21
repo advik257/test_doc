03f33fbca64522ad78f12b7b3c8466c0d8411647
advik257 <Adviktest99@gmail.com>
2025-08-21 07:43:47 +0530

    Clean repository and remove large files

--- README.md
+++
@@ -1,5 +0,0 @@
-# minimum requirement for this project
-# LLM Model- ex.groq(free) or Gemini(15 day free) or HuggingFace(free) or openai(paid) and ollama(local).
-# Embedding Model -openai(paid), HuggingFace,gemini
-# Vector database - In Memory , on disk , cloud base 
-# using HuggingFace embeddings
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='Documentportal',  # Replace with your package name
-    version='0.1.0',  # Replace with your package version
-    author='Venkatesh',  # Replace with your name
-    packages=find_packages(),  # Automatically find packages in the current directory
-)
-
